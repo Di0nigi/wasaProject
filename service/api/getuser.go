bfb5cab0b2e1f35e4483b7ea7402ca13229151ac
Di0nigi <dionigi.ex3@gmail.com>
2024-03-10 16:15:11 +0100

    getuser done

--- service/api/getuser.go
+++ service/api/getuser.go
@@ -1,6 +1,6 @@
 package api
 import (
-	//"encoding/json"
+	"encoding/json"
 	"net/http"
 	
 	"git.sapienzaapps.it/fantasticcoffee/fantastic-coffee-decaffeinated/service/api/reqcontext"
@@ -10,6 +10,14 @@ import (
 
 //etHelloWorld is an example of HTTP endpoint that returns "Hello world!" as a plain text
 func (rt *_router) getUser(w http.ResponseWriter, r *http.Request, ps httprouter.Params, ctx reqcontext.RequestContext) {
-	//id:=ps.ByName("AccountId")
+	id:=ps.ByName("AccountId")
+	err, userP := rt.db.GetUserProfile(id)
+	if err !=nil{
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	w.Header().Set("content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	_ = json.NewEncoder(w).Encode(userP)
 
 }
--- service/database/getuserprofile.go
+++ service/database/getuserprofile.go
@@ -20,5 +20,37 @@ func (db *appdbimpl) GetUserProfile(id string) (error, User) {
 		followers=append(followers,follower)
 	}
 	user.Followers=followers
+	
+
+	var follows []UserId
+	rows, err = db.c.Query("SELECT UserId FROM Followers WHERE FollowedBy=?",id)
+	if err!=nil{
+		return err, user
+	}
+	for rows.Next(){
+		var followed UserId
+		rows.Scan(&followed)
+		follows=append(follows,followed)
+	}
+	user.Follows=follows
+
+	var blocked []UserId
+	rows, err = db.c.Query("SELECT UserId FROM BlockedUsers WHERE BlockedBy=?",id)
+	for rows.Next(){
+		var blockeduser UserId
+		rows.Scan(&blockeduser)
+		blocked=append(blocked,blockeduser)
+	}
+	user.BlockedArray=blocked
+
+	var posts []ObjId
+	rows, err = db.c.Query("SELECT PhotoId FROM PostedImages WHERE ownerId=?",id)
+	for rows.Next(){
+		var post ObjId
+		rows.Scan(&post)
+		posts=append(posts,post)
+	}
+	user.Posts=posts
+
 	return err, user
 }
